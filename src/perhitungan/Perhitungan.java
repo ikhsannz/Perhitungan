5f61f31d6d27fe6937ca0e88d1fe65ec26da5da3
ikhsannz <32305997+ikhsannz@users.noreply.github.com>
2018-07-07 11:03:10 +0800

    membuat inputan dan rumus, dan juga pemindahan scanner(salah tempat)

--- src/perhitungan/Perhitungan.java
+++ src/perhitungan/Perhitungan.java
@@ -5,19 +5,51 @@ import java.util.Scanner;
 
 public class Perhitungan {
 
+
+
+    public static void main(String[] args) {
         Scanner sc1 = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
         Scanner sc3 = new Scanner(System.in);
-
-    public static void main(String[] args) {
-
 
         System.out.println("Aplikasi perhitungan");  
         System.out.println("1. menghitung kecepatan (km/jam)");
         System.out.println("2. menghitung jarak (km)");     
         System.out.println("3. menghitung waktu (jam)");   
-
- 
+        System.out.print("silakan pilih perhitungan mana yang mau dihitung (1/2/3):");
+        int input = sc1.nextInt(); 
+        double hasil, nilai1, nilai2;
+        
+        if(input == 1){
+            System.out.println("menghitung kecepatan");
+            System.out.print("masukan jarak(km)");
+                nilai1=sc2.nextDouble();
+            System.out.print("masukan waktu (jam)");
+                nilai2 = sc3.nextDouble();
+            hasil=nilai1/nilai2;
+            System.out.println("kecepatan anda adalah :"+hasil+" km/jam");
+        }
+        else if(input == 2){
+            System.out.println("menghitung Jarak");
+            System.out.print("masukan Kecepatan(km/jam)");
+                nilai1=sc2.nextDouble();
+            System.out.print("masukan waktu (jam)");
+                nilai2 = sc3.nextDouble();
+            hasil=nilai1*nilai2;
+            System.out.println("Jarak anda adalah :"+hasil+" km");
+        }
+        else if(input == 3){
+            System.out.println("menghitung Waktu");
+            System.out.print("masukan jarak(km)");
+                nilai1=sc2.nextDouble();
+            System.out.print("masukan waktu (jam)");
+                nilai2 = sc3.nextDouble();
+            hasil=nilai1/nilai2;
+            System.out.println("waktu anda adalah :"+hasil+" jam");
+        }
+        else{
+            System.out.println("Maaf, Anda salah memasukan pilihan, cukup pilih dengan angka 1, atau 2, atau 3 !");
+        }
     }
     
 }
